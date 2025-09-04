7244b727ca877cd62e88e85eb43d9d33311954b1
Bilatchuk Ivan <ivanquade07@gmail.com>
2025-09-04 22:49:07 +0300

    findMax

---
+++ Lab01/Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Conditionals
+{
+    static void Main()
+    {
+        int choice = int.Parse(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+            {   
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int c = int.Parse(Console.ReadLine());
+                int max = 0;
+                if (a > b && a > c)
+                {
+                    max = a;
+                }    
+                else if (b > a && b > c)
+                {
+                    max = b;
+                }
+                else
+                {
+                    max = c;   
+                }
+                Console.WriteLine("Max is " + max);
+                break;
+            }    
+        }   
+    }
+}
