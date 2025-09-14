94d2bf6452ea5330d38379f127e18472f156244d
Bilatchuk Ivan <ivanquade07@gmail.com>
2025-09-14 09:50:31 +0300

    task 6-7 completed

---
+++ Lab02/Task6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int n = input.Length;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(input[i]);
+        }
+
+        int start = 0;
+        int len = 1;
+        int bestStart = 0;
+        int bestLen = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] == arr[i - 1])
+            {
+                len++;
+            }
+            else
+            {
+                start = i;
+                len = 1;
+            }
+
+            if (len > bestLen)
+            {
+                bestLen = len;
+                bestStart = start;   
+            }    
+        }
+
+        for (int i = bestStart; i < bestStart + bestLen; i++)
+        {
+            Console.Write(arr[i] + " ");   
+        }    
+        Console.WriteLine();
+    }
+}
