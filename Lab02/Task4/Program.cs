270c819f930e945dd73f43009f92d41d97a440f1
Bilatchuk Ivan <ivanquade07@gmail.com>
2025-09-14 07:41:39 +0300

    task 4 completed

---
+++ Lab02/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        bool[] primes = new bool[n + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            primes[i] = true;
+        }
+        primes[0] = false;
+        primes[1] = false;
+        for (int i = 2; i <= n; i++)
+        {
+            if (primes[i])
+            {
+                Console.Write(i + " ");
+                for (int j = i * 2; j <= n; j += i)
+                {
+                    primes[j] = false;
+                }    
+            }    
+        }    
+    }
+}
