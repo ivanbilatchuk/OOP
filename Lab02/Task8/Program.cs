a12bbca47325de0bbcee34487196cc33a4bdcf09
Bilatchuk Ivan <ivanquade07@gmail.com>
2025-09-14 10:04:09 +0300

    task 8 completed

---
+++ Lab02/Task8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        const int maxVal = 65535;
+        const int minVal = 0;
+        
+        int[] arr = new int[input.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            arr[i] = int.Parse(input[i]);
+
+            if (arr[i] < minVal || arr[i] > maxVal)
+            {
+                Console.WriteLine("Invalid number (it can be only from 0 to 65535).");
+                return;   
+            }    
+        }
+
+        int[] arr2 = new int[maxVal];
+        int max = 0;
+        int mostFreq = -1;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int num = arr[i];
+            arr2[num]++;
+            if (arr2[num] > max)
+            {
+                max = arr2[num];
+                mostFreq = num;
+            }    
+        }    
+        Console.WriteLine($"Number {mostFreq} is the most frequent and occurs {max} times.");
+    }
+}
