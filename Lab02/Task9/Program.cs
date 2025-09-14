9ec0ce223f323a980577f0a65f71fc56845b8678
Bilatchuk Ivan <ivanquade07@gmail.com>
2025-09-14 10:11:06 +0300

    task 9 completed

---
+++ Lab02/Task9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string word = Console.ReadLine();
+        foreach (char c in word)
+        {
+            if (c >= 'a' && c <= 'z')
+            {
+                int index = c - 'a';
+                Console.WriteLine($"{c} -> {index}");  
+            }
+            else if (c >= 'A' && c <= 'Z')
+            {
+                int index = c - 'A';
+                Console.WriteLine($"{c} -> {index}");  
+            }
+            else
+            {
+                Console.WriteLine($"{c} -> isn't a letter");
+            }
+        }    
+    }
+}
