bee39872e3788403a3c56ae295571bd1bdc35a52
Bilatchuk Ivan <ivanquade07@gmail.com>
2025-09-28 15:40:02 +0300

    added try-catch for method main

--- Lab05/Task1/Program.cs
+++ Lab05/Task1/Program.cs
@@ -6,15 +6,23 @@
     {
         static void Main(string[] args)
         {
-            string name = Console.ReadLine();
-            int age = int.Parse(Console.ReadLine());
+            try
+            {
+                string name = Console.ReadLine();
 
-            Chicken chicken = new Chicken(name, age);
-            Console.WriteLine(
-                "Chicken {0} (age {1}) can produce {2} eggs per day.",
-                chicken.Name,
-                chicken.Age,
-                chicken.ProductPerDay);
+                int age = int.Parse(Console.ReadLine());
+
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine(
+                    "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                    chicken.Name,
+                    chicken.Age,
+                    chicken.ProductPerDay);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
