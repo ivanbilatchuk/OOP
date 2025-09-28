0c8c6fda6c72049a82e7a69a85c2c048c9caa4d9
Bilatchuk Ivan <ivanquade07@gmail.com>
2025-09-28 17:11:47 +0300

    task 3 done

--- Lab05/Task3/Program.cs
+++ Lab05/Task3/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿namespace Task3
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Pizza pizza = CreatePizza();
+                ReadToppings(pizza);
+                PrintPizza(pizza);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static Pizza CreatePizza()
+        {
+            string[] pizzaLine = Console.ReadLine().Split(' ');
+            Pizza pizza = new Pizza(pizzaLine[1]);
+            string[] doughLine = Console.ReadLine().Split(' ');
+            Dough dough = new Dough(doughLine[1], doughLine[2], double.Parse(doughLine[3]));
+            pizza.SetDough(dough);
+            return pizza;       
+        }
+
+        static void ReadToppings(Pizza pizza)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] toppingLine = input.Split(' ');
+                Topping topping = new Topping(toppingLine[1], double.Parse(toppingLine[2]));
+                pizza.AddTopping(topping);   
+            }    
+        }
+
+        static void PrintPizza(Pizza pizza)
+        {
+            Console.WriteLine($"{pizza.Name} - {pizza.TotalCalories():f2} Calories");
+        }
+    }    
+}
--- Lab05/Task3/obj/Debug/net9.0/Task3.AssemblyInfo.cs
+++ Lab05/Task3/obj/Debug/net9.0/Task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a4b0a8b3e96dbafda31a2eb05e887887159699b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+581550c00e4fb41ef18287265a16dcdcaaa40e36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
