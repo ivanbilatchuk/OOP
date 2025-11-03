bd734d3d9c6347bea308ab0a0d83f0bf3395bb04
Bilatchuk Ivan <ivanquade07@gmail.com>
2025-11-03 12:21:13 +0200

    task 3done

---
+++ Lab10/Task3/Person.cs
@@ -0,0 +1,30 @@
+namespace Task3;
+using System;
+using System.Collections.Generic;
+
+public class Person : IComparable<Person>
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Town { get; set; }
+
+    public Person(string name, int age, string town)
+    {
+        Name = name;
+        Age = age;
+        Town = town;
+    }
+
+    public int CompareTo(Person other)
+    {
+        if (other == null) return 1;
+
+        int result = this.Name.CompareTo(other.Name);
+        if (result != 0) return result;
+
+        result = this.Age.CompareTo(other.Age);
+        if (result != 0) return result;
+
+        return this.Town.CompareTo(other.Town);
+    }
+}
---
+++ Lab10/Task3/Program.cs
@@ -0,0 +1,41 @@
+﻿using Task3;
+
+class Program
+{
+    static void Main()
+    {
+        List<Person> people = new List<Person>();
+
+        string input;
+        while ((input = Console.ReadLine()) != "END")
+        {
+            string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string name = parts[0];
+            int age = int.Parse(parts[1]);
+            string town = parts[2];
+
+            people.Add(new Person(name, age, town));
+        }
+
+        int n = int.Parse(Console.ReadLine()) - 1;
+        Person target = people[n];
+
+        int equalCount = 0;
+
+        foreach (var p in people)
+        {
+            if (target.CompareTo(p) == 0)
+                equalCount++;
+        }
+
+        if (equalCount == 1)
+        {
+            Console.WriteLine("No matches");
+        }
+        else
+        {
+            int notEqualCount = people.Count - equalCount;
+            Console.WriteLine($"{equalCount} {notEqualCount} {people.Count}");
+        }
+    }
+}
