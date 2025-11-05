d6c34c2f2f85c068f405eb510fd020db6d8a5fae
Bilatchuk Ivan <ivanquade07@gmail.com>
2025-11-05 18:51:39 +0200

    task4 done

---
+++ Lab13/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿using Task4;
+
+class Program
+{
+    static void Main()
+    {
+        List<Student> students = new List<Student>();
+        string input;
+
+        while ((input = Console.ReadLine()) != "END")
+        {
+            string[] parts = input.Split(' ');
+            students.Add(new Student
+            {
+                FirstName = parts[0],
+                LastName = parts[1]
+            });
+        }
+
+        var result = students.OrderBy(s => s.LastName).ThenByDescending(s => s.FirstName); 
+
+        foreach (var s in result)
+        {
+            Console.WriteLine($"{s.FirstName} {s.LastName}");
+        }
+    }
+}
---
+++ Lab13/Task4/Student.cs
@@ -0,0 +1,7 @@
+namespace Task4;
+
+class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
