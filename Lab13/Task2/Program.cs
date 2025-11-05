5ee129e3a46fc926ac303705f9d1d28f9217408d
Bilatchuk Ivan <ivanquade07@gmail.com>
2025-11-05 18:39:07 +0200

    task2 done

---
+++ Lab13/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Task2;
+class Program
+{
+    static void Main()
+    {
+        List<Student> students = new List<Student>();
+        string input;
+        while ((input = Console.ReadLine()) != "END")
+        {
+            string[] data = input.Split(' ');
+            students.Add(new Student
+            {
+                FirstName = data[0],
+                LastName = data[1],
+            });
+        }
+        var result = students.Where(s => string.Compare(s.FirstName, s.LastName, StringComparison.Ordinal) < 0);
+        foreach (var student in result)
+        {
+            Console.WriteLine($"{student.FirstName} {student.LastName}");
+        }  
+    }
+}
---
+++ Lab13/Task2/Student.cs
@@ -0,0 +1,7 @@
+namespace Task2;
+
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
