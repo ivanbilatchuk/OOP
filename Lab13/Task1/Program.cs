26286303f84ab373840ba695ee19667cdc36eaeb
Bilatchuk Ivan <ivanquade07@gmail.com>
2025-11-05 18:35:34 +0200

    task1 done

---
+++ Lab13/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Task1;
+
+class Program
+{
+    static void Main()
+    {
+        List<Student> students = new List<Student>();
+        string input;
+        while ((input = Console.ReadLine()) != "END")
+        {
+            string[] data = input.Split(' ');
+            students.Add(new Student
+            {
+                FirstName = data[0],
+                LastName = data[1],
+                Group = int.Parse(data[2])   
+            });
+        }    
+        var result = students.Where(s => s.Group == 2).OrderBy(s => s.FirstName);
+        foreach (var student in result)
+        {
+            Console.WriteLine($"{student.FirstName} {student.LastName}");
+        }   
+    }
+}
---
+++ Lab13/Task1/Student.cs
@@ -0,0 +1,8 @@
+namespace Task1;
+
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Group { get; set; }
+}
