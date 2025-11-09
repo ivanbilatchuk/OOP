88962e551ae0b09243296dc8c14ded8f0dac8353
Bilatchuk Ivan <ivanquade07@gmail.com>
2025-11-09 09:52:20 +0200

    lab done

--- Lab14/Task11/Program.cs
+++ Lab14/Task11/Program.cs
@@ -1,1 +1,56 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> guests = Console.ReadLine().Split(' ').ToList();
+        List<Func<string, bool>> filters = new List<Func<string, bool>>();
+
+        string input;
+        while ((input = Console.ReadLine()) != "Print")
+        {
+            string[] parts = input.Split(';');
+            string command = parts[0];
+            string type = parts[1];
+            string parameter = parts[2];
+
+            Func<string, bool> filter = null;
+
+            if (type == "Starts with")
+            {
+                filter = name => name.StartsWith(parameter);
+            }
+            else if (type == "Ends with")
+            {
+                filter = name => name.EndsWith(parameter);
+            }
+            else if (type == "Length")
+            {
+                filter = name => name.Length == int.Parse(parameter);
+            }
+            else if (type == "Contains")
+            {
+                filter = name => name.Contains(parameter);
+            }
+
+            if (command == "Add filter")
+            {
+                filters.Add(filter);
+            }
+            else if (command == "Remove filter")
+            {
+                filters.RemoveAll(f => f.Method.ToString() == filter.Method.ToString());
+            }
+        }
+
+        foreach (var filter in filters)
+        {
+            guests = guests.Where(name => !filter(name)).ToList();
+        }
+
+        Console.WriteLine(string.Join(" ", guests));
+    }
+}
---
+++ Lab14/Task12/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> gems = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        List<Func<int, int, bool>> filters = new List<Func<int, int, bool>>();
+        string input;
+
+        while ((input = Console.ReadLine()) != "Forge")
+        {
+            string[] parts = input.Split(';');
+            string command = parts[0];
+            string filterType = parts[1];
+            int parameter = int.Parse(parts[2]);
+
+            Func<int, int, bool> filter = (index, value) =>
+            {
+                int left;
+                if (index > 0)
+                {
+                    left = gems[index - 1];
+                }
+                else
+                {
+                    left = 0;
+                }
+
+                int right;
+                if (index < gems.Count - 1)
+                {
+                    right = gems[index + 1];
+                }
+                else
+                {
+                    right = 0;
+                }
+
+
+                if (filterType == "Sum Left")
+                {
+                    return left + value == parameter;
+                }
+
+                if (filterType == "Sum Right")
+                {
+                    return value + right == parameter;
+                }
+
+                if (filterType == "Sum Left Right")
+                {
+                    return left + value + right == parameter;
+                }
+                return false;
+            };
+
+            if (command == "Exclude")
+            {
+                filters.Add(filter);
+            }
+            else if (command == "Reverse")
+            {
+                filters.RemoveAll(f => f.Method.ToString() == filter.Method.ToString());
+            }
+        }
+
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < gems.Count; i++)
+        {
+            bool excluded = false;
+            foreach (var filter in filters)
+            {
+                if (filter(i, gems[i]))
+                {
+                    excluded = true;
+                    break;
+                }
+            }
+
+            if (!excluded)
+            {
+                result.Add(gems[i]);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
